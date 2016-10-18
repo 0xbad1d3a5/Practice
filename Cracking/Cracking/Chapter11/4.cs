ed7f49ea780c78183d411eecc10c94fa9497e7c6
0x1bad1d3a <0x1bad1d3a@gmail.com>
2016-10-17 22:58:02 -0700

    remove suo

---
+++ Cracking/Cracking/Chapter11/4.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cracking.Chapter11
+{
+    class _4
+    {
+    }
+
+    [TestClass]
+    public class Tests_11_1
+    {
+        [TestMethod]
+        public void Test() 
+        {
+        }
+    }
+}
