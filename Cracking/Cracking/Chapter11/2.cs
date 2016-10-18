85f2ccfd8f63482c28b2836f780de65df6610d23
0x1bad1d3a <0x1bad1d3a@gmail.com>
2016-10-17 22:53:54 -0700

    ignore

---
+++ Cracking/Cracking/Chapter11/2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cracking.Chapter11
+{
+    class _2
+    {
+    }
+
+    [TestClass]
+    public class Tests_11_1
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+    }
+}
