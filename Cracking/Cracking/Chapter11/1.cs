56767457a1f20816ebdb1d2388d2c031fe78e9b2
0x1bad1d3a <0x1bad1d3a@gmail.com>
2016-10-17 22:20:41 -0700

    a

---
+++ Cracking/Cracking/Chapter11/1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * You are given two sorted arrays, A and B, where A has a large enough buffer at
+ * the end to hold B. Write a method to merge B into A in sorted order.
+ */
+namespace Cracking.Chapter11
+{
+    class _1
+    {
+        public static void mergeSortedArrays(int[] longer, int[] shorter, int longerEnd)
+        {
+            for (int i = 0; i < longer.Length; i++)
+            {
+
+            }
+        }
+    }
+
+    [TestClass]
+    public class Tests_11_1
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+
+        }
+    }
+}
