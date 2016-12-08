0d74ebdff3b5742f8395db01915b2b566c7b5565
0x1bad1d3a <0x1bad1d3a@gmail.com>
2016-12-07 22:43:49 -0800

    forgot about comment

--- Cracking/Cracking/Chapter05/02.cs
+++ Cracking/Cracking/Chapter05/02.cs
@@ -12,6 +12,26 @@ using System.Threading.Tasks;
  */
 namespace Cracking.Chapter05
 {
+    /*
+     * This is not the IEEE float representation, but rather binary decimals, i.e. 0.75 = 0.11 (0.1 = 0.5, 0.01 = 0.25)
+     * 
+     * Since converted to binary, 0.75 = 0.11
+     * 
+     * 0.75            = 0.110
+     * 0.75 x 2 = 1.50 = 1.100
+     * 1.50 x 2 = 3.00 = 11.00
+     * 3.00 x 2 = 6.00 = 110.0
+     * 
+     * Therefore, the act of multiplying a number by 2 has the effect of shifting the decimal place to the right by 1
+     * in base 2, much like how multiplying a number by 10 has the effect of shifting the decimal place right by 1 in
+     * base 10.
+     * 
+     * Therefore, we can get the binary representation by:
+     * 
+     * 1) Multiply by 2, if result >= 1 (1 is 1 regardless if you're in base 2 or base 10) then the next digit is a 1,
+     *    otherwise 0. If the result is >= 1 subtract by 1.
+     * 2) Repeat step 1 until result is 0 or can't be represented (>32 characters).
+     */
     class _02
     {
         public static String toBinaryRep(StringBuilder rep, double value)
