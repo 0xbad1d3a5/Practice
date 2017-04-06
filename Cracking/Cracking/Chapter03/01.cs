d4a7069928c6a6439e173bd29c22727d7624f3a6
0x1bad1d3a <0x1bad1d3a@gmail.com>
2017-04-06 09:49:02 -0700

    a

--- Cracking/Cracking/Chapter03/01.cs
+++ Cracking/Cracking/Chapter03/01.cs
@@ -10,41 +10,93 @@ using System.Threading.Tasks;
  */
 namespace Cracking.Chapter03
 {
-    class _01
+    /*
+     * This solution is the simplier one, where we just split the array into 3 even pieces
+     */
+    class NaiveMultiStack
     {
-        static int stackCount = 3;
-        static int stackSize = 2;
-        static int[] buffer = new int[stackSize * stackCount];
-        static int[] stackPointer = Enumerable.Repeat(-1, stackCount).ToArray();
+        private static int stackCount = 3;
+        private int[] stackPointer = Enumerable.Repeat(-1, stackCount).ToArray();
 
-        public static void push(int stack, int value)
+        private int stackSize;
+        private int[] buffer;
+
+        public NaiveMultiStack(int stackSize)
         {
+            this.stackSize = stackSize;
+            this.buffer = new int[stackSize * stackCount];
+        }
+
+        public void push(int stack, int value)
+        {
+            if (stack < 0 || stack > stackCount)
+            {
+                throw new IndexOutOfRangeException();
+            }
             if (stackPointer[stack] >= stackSize - 1)
             {
                 throw new IndexOutOfRangeException();
             }
             stackPointer[stack]++;
-            buffer[stackPointer[stack]] = value;
+            buffer[stackPointer[stack] + stack * stackSize] = value;
         }
 
-        public static int pop(int stack)
+        public int pop(int stack)
         {
             if (stackPointer[stack] < 0)
             {
                 throw new IndexOutOfRangeException();
             }
+            int temp = buffer[stackPointer[stack] + stack * stackSize];
             stackPointer[stack]--;
-            return buffer[stackPointer[stack] + 1];
+            return temp;
         }
     }
 
     [TestClass]
     public class Tests_03_01
     {
+        private static int stackSize = 2;
+
         [TestMethod]
-        public void Test(){
-            _01.push(0, 1);
-            _01.push(0, 1);
+        public void Test1(){
+
+            NaiveMultiStack multiStack = new NaiveMultiStack(stackSize);
+
+            multiStack.push(0, 0);
+            multiStack.push(0, 0);
+            multiStack.push(1, 1);
+
+            Assert.AreEqual(0, multiStack.pop(0));
+            Assert.AreEqual(1, multiStack.pop(1));
+
+            multiStack.push(2, 2);
+
+            Assert.AreEqual(0, multiStack.pop(0));
+            Assert.AreEqual(2, multiStack.pop(2));
+
+            multiStack.push(0, 1);
+            multiStack.push(0, 1);
+            multiStack.push(1, 2);
+
+            Assert.AreEqual(1, multiStack.pop(0));
+            Assert.AreEqual(2, multiStack.pop(1));
+
+            multiStack.push(2, 3);
+
+            Assert.AreEqual(1, multiStack.pop(0));
+            Assert.AreEqual(3, multiStack.pop(2));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Test2()
+        {
+            NaiveMultiStack multiStack = new NaiveMultiStack(stackSize);
+
+            multiStack.push(0, 0);
+            multiStack.push(0, 0);
+            multiStack.push(0, 0);
         }
     }
 }
