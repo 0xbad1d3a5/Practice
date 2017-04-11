0b0be2cdbf4e8e1c0c133a13bb192f1783d07876
0x1bad1d3a <0x1bad1d3a@gmail.com>
2017-04-11 09:52:14 -0700

    a

--- Cracking/Cracking/Chapter05/07.cs
+++ Cracking/Cracking/Chapter05/07.cs
@@ -16,6 +16,70 @@ namespace Cracking.Chapter05
 {
     class _07
     {
+        public static int getBit(List<int> list, int pos, int bit)
+        {
+            return (list[pos] >> bit) & 1;
+        }
+
+        public static int[] countBitsInPosition(List<int> list, int pos){
+            
+            int[] result = new int[2] {0, 0};
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (getBit(list, i, pos) == 0)
+                {
+                    result[0]++;
+                }
+                else
+                {
+                    result[1]++;
+                }
+            }
+
+            return result;
+        }
+
+        public static int findMissingNumber(List<int> list)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 32; i++)
+            {
+                if (list.Count == 0)
+                {
+                    return result;
+                }
+
+                int[] zeroOneCount = countBitsInPosition(list, i);
+                List<int> newList = new List<int>();
+
+                if (zeroOneCount[1] >= zeroOneCount[0])
+                {
+                    result = result << 1;
+                }
+                else
+                {
+                    result = (result << 1) | 1;
+                }
+
+                for (int k = 0; k < list.Count; k++)
+                {
+                    if (zeroOneCount[1] >= zeroOneCount[0] && getBit(list, k, i) == 0)
+                    {
+                        newList.Add(list[k]);
+                    }
+                    else if (getBit(list, k, i) == 1)
+                    {
+                        newList.Add(list[k]);
+                    }
+                }
+
+                list = newList;
+            }
+
+            return -1;
+        }
     }
 
     [TestClass]
@@ -24,7 +88,8 @@ namespace Cracking.Chapter05
         [TestMethod]
         public void Test()
         {
-
+            List<int> list = new List<int> { 0, 1, 2, 4, 5 };
+            Assert.AreEqual(3, _07.findMissingNumber(list));
         }
     }
 }
